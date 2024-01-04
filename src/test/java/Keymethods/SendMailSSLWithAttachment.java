806270272c849a6aa70b0dc8e2c69d6fba6b834e
Vakilsearch <Vakilsearch@DESKTOP-R2AJQE8>
2024-01-04 11:18:58 +0530

    update

--- src/test/java/Keymethods/SendMailSSLWithAttachment.java
+++ src/test/java/Keymethods/SendMailSSLWithAttachment.java
@@ -77,7 +77,7 @@ public void main() throws EmailException{
 
 			// Set the recipient address
 	//message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("sakthi.priyan@vakilsearch.com"));
-         			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("sakthi.priyan@vakilsearch.com,sakthipriyan071297@gmail.com,senthilkumar@vakilsearch.com,kavitha.jagatheeswaran@vakilsearch.com,saravanan.muthu@vakilsearch.com,selvamani@vakilsearch.com,aejaaz.ghouse@vakilsearch.com,naveen.nagalingam@vakilsearch.com,divya.vijayaraghavan@vakilsearch.com"));
+         			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("sakthi.priyan@vakilsearch.com,senthilkumar@vakilsearch.com,kavitha.jagatheeswaran@vakilsearch.com,saravanan.muthu@vakilsearch.com,selvamani@vakilsearch.com,aejaaz.ghouse@vakilsearch.com,naveen.nagalingam@vakilsearch.com,divya.vijayaraghavan@vakilsearch.com"));
 
                      // Add the subject link
 			message.setSubject("LeadCreation 1st set automation");
